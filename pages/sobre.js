0050c8c00259e6472b060ed8a0748f0c3102caaa
Diogo Oliveira <dodo.alexandro@gmail.com>
2021-03-03 02:55:29 -0300

    teste de Link

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+function Sobre() {
+    return (<div>
+        <h1>Sobre</h1>
+        <h2>Diogo Oliveira - 22 Anos</h2> 
+        <Link href="/">
+            <a> Acessar página home</a> 
+        </Link>
+    </div>)
+}
+
+export default Sobre
